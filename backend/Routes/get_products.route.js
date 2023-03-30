59c4a72c94053a917a84fc19ed26827fc670505b
Amit bhandari <a0423355@gmail.com>
2023-03-30 12:54:50 +0530

    basic routes are done

--- backend/Routes/get_products.route.js
+++ backend/Routes/get_products.route.js
@@ -7,14 +7,23 @@ getRouter.get("/",async(req,res)=>{
     console.log(q)
     try {
         if(!req.query.price){
-
             let data =  await ProductModel.find({category:req.query.category})
             res.send(data)
-        }else if(req.query.price){
-            
+        }else if(req.query.price){ 
             let data =  await ProductModel.find({category:req.query.category}).sort({price:req.query.price})
             res.send(data)
         }
+    } catch (error) {
+        res.send({"msg":error.messege})
+    }
+})
+getRouter.get("/:id",async(req,res)=>{
+    const {id} =  req.params
+    console.log(id)
+    try {
+            let data =  await ProductModel.find({_id:id})
+            res.send(data)
+       
     } catch (error) {
         res.send({"msg":error.messege})
     }
