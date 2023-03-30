598df29e28e8a8c2a22154d4474d89ed8f815e15
akshay kumar <akshay123332@gmail.com>
2023-03-30 12:08:33 +0530

    changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Signupmodal from './Components/Signupmodal';
+import Signupmodal from './components/Signupmodal';
 
 function App() {
   return (
     <div className="App">
+      
         <Signupmodal/>
     </div>
   );
