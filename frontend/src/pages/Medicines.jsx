d84efd71915bceb853c65cfe01611c67b76b5abc
Sameerpadhy50 <sameerpadhy50@gmail.com>
2023-04-02 03:17:32 +0530

    Detail Page and Filter added

--- frontend/src/pages/Medicines.jsx
+++ frontend/src/pages/Medicines.jsx
@@ -2,114 +2,160 @@ import React from "react";
 import { useEffect } from "react";
 
 import Filter from "../components/Filter";
+import { StarIcon } from '@chakra-ui/icons'
 
-import { Link, useLocation, useNavigate, useSearchParams } from "react-router-dom";
+import {
+  Link,
+  useLocation,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
 
-import MedicineCard from "../components/MedicineCard";
+
 import { useState } from "react";
 import axios from "axios";
-import { Heading ,Text} from "@chakra-ui/react";
-
+import { Box, Heading, Popover, PopoverTrigger, SimpleGrid, Text } from "@chakra-ui/react";
 
 const Medicines = () => {
- 
-  const[medicine,setMedicine]=useState([])
+  const [medicine, setMedicine] = useState([]);
 
-  const getData=()=>{
-    axios.get("http://localhost:8085/medicines").then((res)=>{
-      setMedicine(res.data)
-    }).catch(err=>{console.log(err)})
-  }
+  const getData = (params) => {
+    axios
+      .get("https://shy-pear-raven-cap.cyclic.app/getdata",params)
+      .then((res) => {
+        setMedicine(res.data);
+        // console.log(res.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  useEffect(()=>{
-    getData()
-  },[])
- console.log(medicine)
+  const [searchParams] = useSearchParams();
+  const location = useLocation();
 
- //const navigate=useNavigate();
-// onClick={()=>navigate("/``")}
- 
+  useEffect(() => {
+    let getMedicineParams;
+    if (location.search || medicine.length === 0) {
+      getMedicineParams = {
+        params: {
+          category: searchParams.getAll("category"),
+          price:searchParams.get("price")
+        },
+      };
+    }
+    getData(getMedicineParams);
+  }, [location.search, medicine.length, searchParams]);
+
+  const navigate = useNavigate();
+  const handleclick = (_id) => {
+    navigate(`/getdata/${_id}`);
+  };
 
   return (
-    <div data-testid="watches" style={{ display: "flex" ,width:"80%",margin:"auto",backgroundColor:"white"}} >
-      <Filter />
+    <div
      
-      <div 
-      
       style={{
-        border:"3px solid Teal",
+        border:"3px solid red",
+        display: "flex",
         width: "100%",
-        display: "grid",
-        gap: "10px",
-        gridTemplateColumns: "repeat(auto-fit, minmax(310px, max-content)",
-        justifyContent: "center",
         
+        margin: "auto",
+        backgroundColor: "#F6F6F7",
       }}
-      >
-        {
-         medicine.length>0 &&  medicine?.map((watch) => {
-            return (
-              // <div key={watch.id} style={{ width: "310px" }}>
-              //     {/* <Link 
-              //       to={`/medicine/${watch.id}`}
-              //       style={{ textDecoration: "none", color: "black" }}
-              //     > */}
-              //        <MedicineCard watch={watch} /> 
-              //     {/* </Link> */}
-              // </div>
-              
-              <div className="main_div"
-        style={{
-          display: "block",
-          width: "80%",
-          height: "400px",
-          border: "2px solid black",
-          borderRadius: "2px",
-          boxShadow:'2xl',
-          margin:"15"
+    >
+      <div>
+      <Filter />
+      </div>
 
+      <SimpleGrid
+        columns={[1, 2, 2, 4]} gap={5}
+        style={{
+          width: "80%",
+          margin: "auto",
+          // display: "grid",
+          gap: "10px",
+          // gridTemplateColumns: "repeat(auto-fit, minmax(400px, min-content)",
+          justifyContent: "center",
+          border:"3px solid teal",
+          
         }}
       >
-        <div 
-          style={{
-            display: "flex",
-            height: "50%",
-            padding: "20px 20px 0 20px",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-        >
-          <img 
-            alt="watch"
-            src={watch.image}
-            style={{
-              backgroundSize: "contain",
-              width: "100%",
-              maxHeight: "100%",
-            }}
-          />
-        </div>
-        
-        <div style={{ textAlign: "start" }} >
-          <div  style={{ fontSize: "20px" }} > {watch.name}</div>
-          <p>bottel of 60 capsules</p>
-          <p>MRP:<span><s>{watch.price}</s></span> <span>{watch.discount}</span></p>
+        {medicine.length > 0 &&
+          medicine?.map((watch) => {
+            return (
+              <Popover> 
+                <PopoverTrigger>
+              <Box
+              key={watch._id}
+                className="main_div"
+                boxShadow="md"
+                onClick={() => handleclick(watch._id)}
+                style={{
+                  display: "block",
+                  width: "90%",
+                  height: "400px",
+                  //  border: "2px solid black",
+                  borderRadius: "2px",
+                  marginTop: "50px",
+                  padding: "10px",
+                  backgroundColor:"#FCFCFC",
+                  boxShadow:"xl"
+                }}
+              >
+                <Box
+                  style={{
+                    width:"70%",
+                    alignItems:"center",
+                    justifyContent:"center",
+                    display: "flex",
+                    height: "60%",
+                    padding: "20px 20px 0 20px",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    backgroundColor:"#FCFCFC",
+                    border:"1px solid red"
+                  }}
+                >
+                  <img
+                    alt="watch"
+                    src={watch.image}
+                    style={{
+                      backgroundSize: "contain",
+                      width: "100%",
+                      maxHeight: "100%",
+                    }}
+                  />
+                </Box>
+                <Box style={{ textAlign: "start" }}>
+                  <Heading  as='h6' size='sm'> {watch.name}</Heading>
+                  <br />
+                  <Text fontSize='xs'>bottel of 60 capsules</Text >
+                  <p>{watch.category}</p>
+                  
+                  <Text fontSize='sm'>
+                    MRP:
+                    <span>
+                      <s>{watch.mainprice}</s>
+                    </span>{" "}
+                    <span style={{ color: "red" }}>{watch.discount}</span>
+                  </Text >
 
-            <div style={{display:"flex"}}>
-
-          <Text fontSize="xl">{watch.mainprice}</Text>
-          <button style={{paddingLeft:"100px",color:"red"}} ><b>ADD</b></button>
-            </div>
-
-        </div>
-      </div>
-            )
+                  <div style={{ display: "flex" }}>
+                    <Text fontSize="xl">Rs {watch.price}</Text>
+                    <button style={{ paddingLeft: "100px", color: "red" }}>
+                      <b>ADD</b>
+                    </button>
+                  </div>
+                </Box>
+              </Box>
+              </PopoverTrigger>
+              </Popover>
+            );
           })}
-      </div>
+      </SimpleGrid>
 
-      {/* {medicine?.map((el)=><div>
-      <img src={el.image} /></div>)} */}
-
+      
     </div>
   );
 };
--- frontend/src/pages/SinglePage.jsx
+++ frontend/src/pages/SinglePage.jsx
@@ -4,34 +4,159 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect } from "react";
 
-const SinglePage = ( ) => {
+//import React from 'react';
+import {
+  Box,
+  Heading,
+  Link,
+  Image,
+  Text,
+  Divider,
+  HStack,
+  Tag,
+  Wrap,
+  WrapItem,
+  SpaceProps,
+  useColorModeValue,
+  Container,
+  VStack,
+  Stack,
+  Button,
+  Tab,
+} from "@chakra-ui/react";
 
-  const {id}  = useParams();
-  const [data, setData]  = useState({});
-  // const getData = ()=>{
-  //   axios.get(`http://localhost:8080/medicines/${id}`).then((r)=>{
-  //     setData(r.data);
-  //   }).catch(()=>{
-
-  //   })
-  // }
-
-  // console.log(data);
-  // useEffect(()=>{
-  //   getData()
-  // },[])
-
+const BlogTags = (props) => {
   return (
-    <div>
-      <h2>Watch name</h2>
-      <div>
-        <img src={data.image} alt="Cover Pic" />
-      </div>
-      <div>
-        <div><h2>{data.category}</h2></div>
-      </div>
-    </div>
+    <HStack spacing={2} marginTop={props.marginTop}>
+      {props.tags.map((tag) => {
+        return (
+          <Tag size={"md"} variant="solid" colorScheme="orange" key={tag}>
+            {tag}
+          </Tag>
+        );
+      })}
+    </HStack>
   );
 };
 
-export default SinglePage;
+export const BlogAuthor = (props) => {
+  return (
+    <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
+      <Image
+        borderRadius="full"
+        boxSize="40px"
+        src="https://100k-faces.glitch.me/random-image"
+        alt={`Avatar of ${props.name}`}
+      />
+      <Text fontWeight="medium">{props.name}</Text>
+      <Text>—</Text>
+      <Text>{props.date.toLocaleDateString()}</Text>
+    </HStack>
+  );
+};
+ 
+const SinglePage = () => {
+  const { id } = useParams();
+  const [data, setData] = useState({});
+ 
+  const getData = (id) => {
+
+    axios.get(`https://shy-pear-raven-cap.cyclic.app/getdata/${id}`)
+  .then((res) => {
+        setData(res.data[0]);
+
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // console.log(data);
+  useEffect(() => {
+    getData(id);
+  }, [id]);
+
+  return (
+    <Container maxW={"6xl"} p="12" border="1px solid red">
+      {/* <Heading as="h1">Stories by Chakra Templates</Heading> */}
+      <Box
+        marginTop={{ base: "1", sm: "5" }}
+        display="flex"
+        flexDirection={{ base: "column", sm: "row" }}
+        justifyContent="space-between"
+      >
+        <Box
+          color="#FFFFFF"
+          display="flex"
+          flex="1"
+          marginRight="3"
+          position="relative"
+          alignItems="center"
+        >
+          <Box
+            width={{ base: "100%", xl: "85%" }}
+            zIndex="2"
+            marginLeft={{ base: "0", sm: "5%" }}
+            marginTop="5%"
+            boxShadow="Base"
+          >
+            <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+              <Image
+                // border="1px solid red"
+                width="60%"
+                borderRadius="lg"
+                src={data.image}
+                alt="some good alt text"
+                objectFit="contain"
+              />
+            </Link>
+          </Box>
+        </Box>
+        <Box
+          ml="-200px"
+          textAlign="start"
+          // border="2px solid red"
+          display="flex"
+          flex="1"
+          flexDirection="column"
+          justifyContent="center"
+          marginTop={{ base: "3", sm: "0" }}
+        >
+          <BlogTags tags={["new"]} />
+          <Heading as="h1" size="lg">
+            {/* <Link textDecoration="none" _hover={{ textDecoration: 'none' }}> */}
+            {data.name}
+            {/* </Link> */}
+          </Heading>
+          <Text
+            as="p"
+            marginTop="2"
+            color={useColorModeValue("gray.700", "gray.200")}
+            fontSize="lg"
+          >
+            {data.brand}
+          </Text>
+          <Text>{data.category}</Text>
+          <Text>{data.subcat2}</Text>
+          <Text>
+            <s>{data.mainprice}</s> <span>{data.discount}</span>
+          </Text>
+          <Text>₹{data.price}</Text>
+          {/* <BlogAuthor name="John Doe" date={new Date('2021-04-06T19:01:27Z')} /> */}
+          <br />
+          <br />
+          <br />
+
+          <Stack direction="row" spacing={4}>
+            <Button w="300px" colorScheme="orange" variant="solid" >
+              Add To Cart
+            </Button>
+          </Stack>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default SinglePage
