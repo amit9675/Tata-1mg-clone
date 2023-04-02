47a9b11f4791a81257f87604439021704204f50f
Sameerpadhy50 <sameerpadhy50@gmail.com>
2023-04-02 22:42:06 +0530

    day-6 data added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
 //import Filter from './components/Filter';
 import MainRoutes from './pages/MainRoutes';
-import Medicines from './pages/Medicines';
-import Admin from './Admin/Admin';
-import MainRoutes from './pages/MainRoutes';
+
+
 
 import CartPage from './pages/Cart';
 
